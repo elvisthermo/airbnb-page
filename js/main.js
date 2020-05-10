ab802d26ead1245c6044d1da72d4ccf77718cf57
elvisthermo <elvis.thermo@gmail.com>
2020-05-10 00:22:17 -0300

    add geolocalização das hospedagens

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 window.onload = async function () {
-    const arbanbiApi = await fetch("https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72")
+    const arbanbiApi = await fetch("https://my.api.mockaroo.com/airbnbgamaacademy.json?key=4fbeba30")
         .then(res => res.json())
         .then(data => {
             return data;
@@ -8,51 +8,58 @@ window.onload = async function () {
             throw err;
         });
 
+    function createCards() {
+        arbanbiApi.map((result) => {
+            let id = `section-${result.property_type}`;
 
+            const container = document.getElementById(id);
 
-    let types = arbanbiApi.map(d => d.property_type);
+            // Create card element
+            const card = document.createElement('div');
+            card.classList = 'card-body';
 
-
-    types = [...new this.Set(types)];
-
-
-    arbanbiApi.map((result) => {
-        let id = `section-${result.property_type}`;
-     
-        const container = document.getElementById(id);
-
-        // Create card element
-        const card = document.createElement('div');
-        card.classList = 'card-body';
-
-        const content = `
-        <div class="cards">
-            <img src="${result.photo}" alt=""
-            >
-            <header class="header-cards"><h3>${result.name.toUpperCase()}</h3></header>
-            <p>Preço da diaria: ${result.price},00 R$</p>
-            
-            <p>${result.property_type}</p>
+            const content = `
+            <div class="cards">
+                <img src="${result.photo}" alt=""
+                >
+                <header class="header-cards"><h3>${result.name.toUpperCase()}</h3></header>
+                <p>Preço da diaria: ${result.price},00 R$</p>
+                
+                <p>${result.property_type}</p>
+            </div>
+    
+          </div>
         </div>
-
       </div>
-    </div>
-  </div>
-  `;
+      `;
 
-        // Append newyly created card element to the container
-        container.innerHTML += content;
-    })
+            // Append newyly created card element to the container
+            container.innerHTML += content;
+        })
+    }
 
-    function selectionClass(type) {
+    function locationMaps() {
+        var mapOptions = {
+            center: new google.maps.LatLng(-34.397, 150.644),
+            zoom: 2.8,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map_canvas"),
+            mapOptions);
+
+
+        arbanbiApi.map((ponto, index) => {
+
+            var marker = new google.maps.Marker({
+                position: new google.maps.LatLng(ponto.latitude, ponto.longitude),
+                title: ponto.name,
+                map: map
+            });
+
+        });
 
     }
 
-    function createCard() {
-        let container = document.getElementById('section-a');
-
-        document.createElement("div")
-
-    }
-
+    createCards();
+    locationMaps();
 }
